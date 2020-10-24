2fbabaf51e9f46e1a4b47c380f5b303565635210
SovanMarat <maratwp@mail.ru>
2020-10-24 11:26:03 +0300

    feat: function morse done

--- src/index.js
+++ src/index.js
@@ -35,10 +35,60 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    " ": " "
 };
 
 function decode(expr) {
-    // write your solution here
+    let str=expr;
+    let strTemp = '';
+    let arrTemp = [];
+    let arrNew = [];
+    let arrN = [];
+    for (let i = 0; i < str.length; i++) {
+        if ((i + 1) % 10 === 0) {
+            strTemp = `${strTemp}${str[i]},`
+        } else {
+            strTemp = `${strTemp}${str[i]}`
+        }
+        arrTemp = strTemp.split(',');
+        arrTemp.length = arrTemp.length - 1;
+        arrNew = arrTemp.map(item => {
+            item = String(item);
+            if (item === '**********') {
+                item = 99;
+                return item;
+            }
+            item = Number(item);
+            return item;
+        });
+    }
+    arrN = arrNew.map(item => {
+        let itemT = '';
+        item = String(item);
+        for (let i = 0; i < item.length; i += 2) {
+            let itemTemp = '';
+            itemTT = (item[i]) + (item[i + 1]);
+            if ((item[i]) + (item[i + 1]) === '10') {
+                itemTemp = `${itemTemp}.`;
+            }
+            if (item[i] + item[i + 1] === '11') {
+                itemTemp = `${itemTemp}-`;
+            }
+            if (item[i] === '9') {
+                itemTemp = `${itemTemp} `;
+            }
+            itemT = `${itemT}${itemTemp}`;
+        }
+        return itemT;
+    });
+let result = [];
+result = arrN.map(item => {
+ item=MORSE_TABLE[item];
+        return item;
+    });
+    result=result.join('');
+    console.log(result);
+    return result;
 }
 
 module.exports = {
